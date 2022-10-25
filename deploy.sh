43b9f06b0f89c21f9e761e93a2d2d259604f03c1
Nihar Paital <nihar.pshellaws@gmail.com>
2022-10-25 13:57:53 +0000

    Added .gitignore file

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This script is going to integrate the CICD pipeline
+
--- integrate.sh
+++ integrate.sh
@@ -3,3 +3,4 @@
 # This code is prepared for integrating the other source codes
 
 cp Product.java Product_`date +'%d-%m-%Y'`.java
+Hello
